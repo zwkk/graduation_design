0201ecff3226e417eed786d64eaf5974bfc92a90
zwk <1328334085@qq.com>
2023-03-13 08:55:10 +0800

    chapter，section

--- src/main/java/graduation/design/controller/ChapterController.java
+++ src/main/java/graduation/design/controller/ChapterController.java
@@ -2,11 +2,17 @@ package graduation.design.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import graduation.design.entity.Chapter;
+import graduation.design.entity.Section;
 import graduation.design.service.ChapterService;
+import graduation.design.service.SectionService;
+import graduation.design.vo.ChapterSectionVo;
 import graduation.design.vo.Result;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -23,11 +29,14 @@ public class ChapterController {
     @Autowired
     ChapterService chapterService;
 
+    @Autowired
+    SectionService sectionService;
+
     @ApiOperation("新增章")
     @PostMapping("/addChapter")
     public Result addChapter(@RequestBody Chapter chapter){
         chapter.setId(null);
-        if(chapterService.getOne(new QueryWrapper<Chapter>().eq("num",chapter.getNum()).eq("version",chapter.getVersion()))!=null){
+        if(chapterService.getOne(new QueryWrapper<Chapter>().eq("num",chapter.getNum()))!=null){
             return Result.fail("该章已存在");
         }
         chapterService.save(chapter);
@@ -53,4 +62,21 @@ public class ChapterController {
         chapterService.saveOrUpdate(chapter);
         return Result.success(null);
     }
+
+    @ApiOperation(value = "获取章节目录",response = ChapterSectionVo.class)
+    @GetMapping ("/getChapter")
+    public Result getChapter(){
+        List<ChapterSectionVo> chapterSectionVoList = new ArrayList<>();
+        List<Chapter> chapterList = chapterService.list();
+        for (Chapter chapter : chapterList) {
+            ChapterSectionVo chapterSectionVo = new ChapterSectionVo();
+            chapterSectionVo.setChapterNum(chapter.getNum());
+            chapterSectionVo.setTitle(chapter.getTitle());
+            List<Section> sectionList = sectionService.list(new QueryWrapper<Section>().eq("chapter_num",chapter.getNum()));
+            chapterSectionVo.setSectionList(sectionList);
+            chapterSectionVoList.add(chapterSectionVo);
+        }
+        return Result.success(chapterSectionVoList);
+    }
+
 }
--- src/main/java/graduation/design/controller/SectionController.java
+++ src/main/java/graduation/design/controller/SectionController.java
@@ -1,12 +1,17 @@
 package graduation.design.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import graduation.design.entity.AuthorSection;
 import graduation.design.entity.Chapter;
 import graduation.design.entity.Section;
+import graduation.design.entity.SectionDetail;
+import graduation.design.service.AuthorSectionService;
 import graduation.design.service.ChapterService;
+import graduation.design.service.SectionDetailService;
 import graduation.design.service.SectionService;
 import graduation.design.vo.ChapterSectionVo;
 import graduation.design.vo.Result;
+import graduation.design.vo.SectionVo;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -32,14 +37,20 @@ public class SectionController {
     @Autowired
     ChapterService chapterService;
 
+    @Autowired
+    SectionDetailService sectionDetailService;
+
+    @Autowired
+    AuthorSectionService authorSectionService;
+
     @ApiOperation("新增节")
     @PostMapping("/addSection")
     public Result addSection(@RequestBody Section section){
         section.setId(null);
-        if(sectionService.getOne(new QueryWrapper<Section>().eq("num",section.getNum()).eq("version",section.getVersion()))!=null){
+        if(sectionService.getOne(new QueryWrapper<Section>().eq("num",section.getNum()).eq("chapter_num",section.getChapterNum()))!=null){
             return Result.fail("该节已存在");
         }
-        if(chapterService.getOne(new QueryWrapper<Chapter>().eq("num",section.getChapterNum()).eq("version",section.getVersion()))==null) return Result.fail("该章不存在");
+        if(chapterService.getOne(new QueryWrapper<Chapter>().eq("num",section.getChapterNum()))==null) return Result.fail("该章不存在");
         sectionService.save(section);
         return Result.success(null);
     }
@@ -57,30 +68,62 @@ public class SectionController {
     @ApiOperation("修改节")
     @PostMapping("/changeSection")
     public Result changeSection(@RequestBody Section section){
-        //作者权限校验
         if(sectionService.getById(section.getId())==null){
-            return Result.fail("该章不存在");
+            return Result.fail("该节不存在");
         }
         sectionService.saveOrUpdate(section);
         return Result.success(null);
     }
 
-    @ApiOperation(value = "根据版本获取教材内容",response = ChapterSectionVo.class)
-    @GetMapping ("/querySection")
-    public Result querySection(String version){
-        List<Chapter> chapterList = chapterService.list(new QueryWrapper<Chapter>().eq("version", version));
-        if(chapterList.size()==0) return Result.fail("未查询到对应版本的教材");
-        List<ChapterSectionVo> chapterSectionVoList = new ArrayList<>();
-        for (Chapter chapter : chapterList) {
-            ChapterSectionVo chapterSectionVo = new ChapterSectionVo();
-            chapterSectionVo.setVersion(version);
-            chapterSectionVo.setNum(chapter.getNum());
-            chapterSectionVo.setTitle(chapter.getTitle());
-            List<Section> sectionList = sectionService.list(new QueryWrapper<Section>().eq("version", version).eq("chapter_num", chapter.getNum()));
-            chapterSectionVo.setSectionList(sectionList);
-            chapterSectionVoList.add(chapterSectionVo);
-        }
-        return Result.success(chapterSectionVoList);
+    @ApiOperation(value = "查询某节版本列表",response = SectionDetail.class)
+    @GetMapping("/querySection")
+    public Result querySection(Integer sectionId){
+        List<SectionDetail> versionList = sectionDetailService.list(new QueryWrapper<SectionDetail>().eq("section_id", sectionId).select("section_id", "version"));
+        return Result.success(versionList);
     }
 
+    @ApiOperation(value = "根据节和版本号查询教材内容",response = SectionVo.class)
+    @GetMapping ("/queryContent")
+    public Result queryContent(Integer sectionId,String version){
+        SectionDetail detail = sectionDetailService.getOne(new QueryWrapper<SectionDetail>().eq("section_id", sectionId).eq("version", version));
+        SectionVo sectionVo = new SectionVo();
+        sectionVo.setContent(detail.getContent());
+        sectionVo.setVersion(detail.getVersion());
+        sectionVo.setNum(sectionService.getById(sectionId).getNum());
+        sectionVo.setTitle(sectionService.getById(sectionId).getTitle());
+        return Result.success(sectionVo);
+    }
+
+    @ApiOperation(value = "根据节查询教材内容,默认最新",response = SectionVo.class)
+    @GetMapping ("/queryNewContent")
+    public Result queryNewContent(Integer sectionId){
+        SectionDetail detail = sectionDetailService.getOne(new QueryWrapper<SectionDetail>().eq("section_id", sectionId).orderByDesc("version").last("limit 1"));
+        SectionVo sectionVo = new SectionVo();
+        sectionVo.setContent(detail.getContent());
+        sectionVo.setVersion(detail.getVersion());
+        sectionVo.setNum(sectionService.getById(sectionId).getNum());
+        sectionVo.setTitle(sectionService.getById(sectionId).getTitle());
+        return Result.success(sectionVo);
+    }
+
+    @ApiOperation("作者编辑教材")
+    @PostMapping ("/editContent")
+    public Result queryNewContent(Integer authorId,Integer sectionId,String content){
+        if(authorSectionService.getOne(new QueryWrapper<AuthorSection>().eq("author_id",authorId).eq("section_id",sectionId))==null) {
+            return Result.fail("无权限编辑");
+        }
+        SectionDetail sectionDetail = new SectionDetail();
+        sectionDetail.setSectionId(sectionId).setContent(content).setAuthorId(authorId);
+        SectionDetail detail = sectionDetailService.getOne(new QueryWrapper<SectionDetail>().orderByDesc("version").last("limit 1"));
+        if(detail==null) {
+            sectionDetail.setVersion("1");
+        }else {
+            sectionDetail.setVersion(String.valueOf(Integer.parseInt(detail.getVersion())+1));
+        }
+        sectionDetailService.save(sectionDetail);
+        return Result.success(null);
+    }
+
+
 }
+
