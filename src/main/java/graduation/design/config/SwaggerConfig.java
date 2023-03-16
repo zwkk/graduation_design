9fe433fb4f21535bdfdb84346639a6cf21d57c9b
林修贤 <1229010597@qq.com>
2023-03-16 11:36:50 +0800

    feat:使用Knife4j

--- src/main/java/graduation/design/config/SwaggerConfig.java
+++ src/main/java/graduation/design/config/SwaggerConfig.java
@@ -1,37 +1,56 @@
 package graduation.design.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    private static ApiInfo DEFAULT = null;
-    @Bean
-    public Docket docket(){
-        Contact DEFAULT_CONTACT = new Contact("zwk", "", "");
-        DEFAULT = new ApiInfo("graduation_design",
-                "实践教材管理平台",
-                "V-1.0",
-                "",
-                DEFAULT_CONTACT,
-                "Apache 2.0",
-                "http://www.apache.org/licenses/LICENSE-2.0",
-                new ArrayList());
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(DEFAULT)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("graduation.design.controller"))
-                .build();
+//    @Bean
+//    public Docket docket(){
+//        Contact DEFAULT_CONTACT = new Contact("zwk", "", "");
+//        DEFAULT = new ApiInfo("graduation_design",
+//                "实践教材管理平台",
+//                "V-1.0",
+//                "",
+//                DEFAULT_CONTACT,
+//                "Apache 2.0",
+//                "http://www.apache.org/licenses/LICENSE-2.0",
+//                new ArrayList());
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(DEFAULT)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("graduation.design.controller"))
+//                .build();
+//
+//        }
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        String groupName="3.X版本";
+        Docket docket=new Docket(DocumentationType.OAS_30)
+            .apiInfo(new ApiInfoBuilder()
+                .title("实践教材管理平台")
+                .description("# 这里记录服务端所有的接口的入参，出参等等信息")
+                .termsOfServiceUrl("")
+                .version("2.0")
+                .build())
+            //分组名称
+            .groupName(groupName)
+            .select()
+            //这里指定Controller扫描包路径
+            .apis(RequestHandlerSelectors.basePackage("graduation.design.controller"))
+            .paths(PathSelectors.any())
+            .build();
+        return docket;
     }
 
 
